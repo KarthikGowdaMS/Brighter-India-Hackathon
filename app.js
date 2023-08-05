bdebe2b61f6d976afafab2871416954ab7814571
karthikgowdams03 <mskarthikgowda03@gmail.com>
2023-08-06 03:04:22 +0530

    Merge frontend and model

--- app.js
+++ app.js
@@ -2,17 +2,20 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const _ = require('lodash');
 const mongoose = require('mongoose');
-const http = require('http');
+const https = require('https');
+// const { connect } = require('http2');
 
 const app = express();
+
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 const date = new Date();
+const port = 3000 || process.env.PORT;
 // const day = date.getDate();
 
-mongoose.connect('mongodb://0.0.0.0:27017/fruitsDB', {useNewUrlParser: true});
+mongoose.connect('mongodb+srv://karthikgowdams27:123@tweets.vqnbjuv.mongodb.net/?retryWrites=true&w=majority', { useNewUrlParser: true });
 
 const postSchema = new mongoose.Schema({
     msg: String,
@@ -22,15 +25,74 @@ const postSchema = new mongoose.Schema({
 const Tweet = mongoose.model('Tweet', postSchema);
 
 
-app.get('/', async function(req, res){
-    res.sendFile(__dirname + "./index.html");
+app.get('/', async function (req, res) {
+    res.sendFile(__dirname + "/intro.html");
 });
 
-app.post('/', async function(req, res){
-    const tweet = new tweet({
-        msg: req.body.message,  
-        msgDate: date.getDate().toString()
+app.post('/', async function (req, res) {
+
+    const data = {
+        "kind": "Conversation",
+        "analysisInput": {
+            "conversationItem": {
+                "id": "1",
+                "participantId": "1",
+                "text": "hi"
+            }
+        },
+        "parameters": {
+            "projectName": "test1",
+            "deploymentName": "test-nlp",
+            "stringIndexType": "TextElement_V8"
+        }
+    }
+
+    // let dataobj;
+
+    const headers = {
+        "Ocp-Apim-Subscription-Key": '2c1c43a0282f482898b92dc3772a0366'
+    };
+    const options = {
+        method: "POST",
+        headers: headers
+    };
+    const request = https.request('https://nlp-test.cognitiveservices.azure.com/language/:analyze-conversations?api-version=2022-10-01-preview', options, (response) => {
+
+        response.on('data', async (data) => {
+
+
+            if (response.statusCode >= 200 && response.statusCode <= 300) {
+
+                const tweet = new Tweet({
+                    msg: req.body.message,
+                    msgDate: date.getDate().toString()
+                });
+
+                await tweet.save();
+                // res.send('success');
+                // console.log("Success");
+                // alert("Success");
+                const dataobj = JSON.parse(data);
+                // console.log(response);
+                res.write(dataobj.result.prediction.intents);
+                res.redirect('/');
+            }
+            else {
+                res.send('failure');
+                console.log("Failure");
+                // alert("Failure");
+            }
+
+
+        });
+
     });
 
-    await tweet.save();
+    request.write(JSON.stringify(data));
+    request.end();
+
+});
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`)
 });
