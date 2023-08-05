38ed43a08f75c8508a5908acf78c7ba924a83869
navaneethjainsl <navaneethjainsl@gmail.com>
2023-08-06 00:29:34 +0530

    create server

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const _ = require('lodash');
+const mongoose = require('mongoose');
+const http = require('http');
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+const date = new Date();
+// const day = date.getDate();
+
+mongoose.connect('mongodb://0.0.0.0:27017/fruitsDB', {useNewUrlParser: true});
+
+const postSchema = new mongoose.Schema({
+    msg: String,
+    msgDate: String
+});
+
+const Tweet = mongoose.model('Tweet', postSchema);
+
+
+app.get('/', async function(req, res){
+    res.sendFile(__dirname + "./index.html");
+});
+
+app.post('/', async function(req, res){
+    
+});
